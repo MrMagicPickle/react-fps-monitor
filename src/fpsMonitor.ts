2e7405990b9662d931338cc43e54b579b8a8ab04
Wei Shen Tan <tanweishen97@gmail.com>
2023-11-14 01:47:03 +0800

    Clean up code

--- src/fpsMonitor.ts
+++ src/fpsMonitor.ts
@@ -1,25 +1,9 @@
-export interface FPSUpdatedEvent extends Event {
-  detail: FPSUpdatedEventDetail
-};
-
-export interface FPSUpdatedEventDetail {
-  fpsCount: number;
-}
-
-const DEBUG = true;
 export class FPSMonitor {
   private fps: number;
-  public maxFPS: number;
-  private eventListener: EventListener | undefined;
 
   constructor() {
     this.fps = 60;
-    this.maxFPS = 100;
     this.initCalculateFPS();
-  }
-
-  setMaxFps(fps: number) {
-    this.maxFPS = fps;
   }
 
   private initCalculateFPS() {
@@ -36,90 +20,16 @@ export class FPSMonitor {
 
       // Update FPS every second
       if (elapsed >= 1000) {
-        this.fps =
-          Math.min(
-            Number((frameCount / (elapsed / 1000)).toFixed(2)),
-            this.maxFPS
-          );
+        this.fps = Number((frameCount / (elapsed / 1000)).toFixed(2));
         frameCount = 0;
         lastTimestamp = timestamp;
       }
-
       requestAnimationFrame(updateFPS);
     }
     requestAnimationFrame(updateFPS);
   }
 
-  public onFPSCalculated(eventHandlerCb: (fpsCount: number) => void) {
-    this.eventListener = (e) => {
-      eventHandlerCb((e as FPSUpdatedEvent).detail.fpsCount);
-    };
-    document.addEventListener('FPSUpdated', this.eventListener);
-  }
-
-
   public getFPS() {
     return this.fps;
   }
-
-  public destroy() {
-    if (!this.eventListener) {
-      return;
-    }
-    console.log('Removing event listener', this.eventListener);
-    document.removeEventListener('FPSUpdated', this.eventListener);
-  }
-
-  /*
-   * -----------------------------------------------
-   * V1 - manual trigger.
-   * -----------------------------------------------
-   */
-  /*
-   * @param timeWindow - duration in ms to measure FPS before emitting event.
-   */
-  public calculateFPS(timeWindow: number = 5000) {
-    let lastTimestamp: number;
-    let frameCount = 0;
-    let startTime = performance.now();
-
-    const updateFPS = (timestamp: number) => {
-      if (!lastTimestamp) {
-        lastTimestamp = timestamp;
-      }
-
-      const elapsed = timestamp - lastTimestamp;
-      frameCount++;
-
-      // Update FPS every second
-      if (elapsed >= 1000) {
-        this.fps = Number((frameCount / (elapsed / 1000)).toFixed(2));
-        frameCount = 0;
-        lastTimestamp = timestamp;
-      }
-
-      // Your rendering logic here
-      let endTime = performance.now();
-
-      // if time window is not reached, continue to render
-      const diff = endTime - startTime;
-      if (diff < timeWindow) {
-        requestAnimationFrame(updateFPS);
-      } else {
-        this.emitFPSUpdatedEvent();
-      }
-    }
-    requestAnimationFrame(updateFPS);
-  }
-
-  private emitFPSUpdatedEvent() {
-    const event = new CustomEvent<FPSUpdatedEventDetail>('FPSUpdated', {
-      detail: {
-        fpsCount: this.fps
-      }
-    });
-    console.log('Emitting event', event);
-    document.dispatchEvent(event);
-  }
-
 }
--- src/useFpsMonitor.ts
+++ src/useFpsMonitor.ts
@@ -3,43 +3,43 @@ import { useState, useEffect, useRef } from "react";
 
 
 export interface DynamicRenderingProps {
-  customLODs?: string[];
+  levelOfDetails?: string[];
   updateInterval: number;
   minFpsThreshold: number;
 }
+
 const defaultLODs = [
   'high',
-  'medium',
   'low',
 ];
+
 export default function useDynamicRendering({
-  customLODs = defaultLODs,
+  levelOfDetails = defaultLODs,
   updateInterval = 1000,
   minFpsThreshold = 30,
 }: DynamicRenderingProps = {
-  customLODs: defaultLODs,
+  levelOfDetails: defaultLODs,
   updateInterval: 1000,
   minFpsThreshold: 30,
 }) {
 
   const [levelOfDetail, setLOD] = useState('');
-  const currLodIndexRef = useRef(-1);
+  const currLodIndexRef = useRef(0);
 
   const resetLOD = () => {
-    currLodIndexRef.current = -1;
+    currLodIndexRef.current = 0;
+    setLOD(levelOfDetails[currLodIndexRef.current]);
   };
 
   useEffect(() => {
-    const possibleLODs = customLODs;
-
     const packageClass = new FPSMonitor();
-    (window as any).pc = packageClass;
 
     const checkFPS = () => {
       const fps = packageClass.getFPS();
+
       if (fps < minFpsThreshold) {
-        currLodIndexRef.current = Math.min(possibleLODs.length - 1, currLodIndexRef.current + 1);
-        setLOD(possibleLODs[currLodIndexRef.current]);
+        currLodIndexRef.current = Math.min(levelOfDetails.length - 1, currLodIndexRef.current + 1);
+        setLOD(levelOfDetails[currLodIndexRef.current]);
       }
     };
 
